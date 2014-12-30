a8eee570c88813b2253ab3d402d36927531f2ae2
Andoni Garcia <andoni@uchicago.edu>
2014-12-30 01:13:49 -0800

    Reversed a string a couple of ways

--- Python/registration.py
+++ Python/registration.py
@@ -1,7 +1,7 @@
 # Andoni Garcia. 2014.
 # My source code for the Registration Phase.
 # Written in Python, because it is my newest language and I like challenges.
-# JK (kinda), I have the most experience with URL requests with Python, because
+# JK (kinda), I have the most experience with URL Requests with Python, because
 # of my new project: https://github.com/andonigarcia/OHHLA-WebScraper
 
 from urllib.request import Request, urlopen
@@ -13,11 +13,11 @@ def Register():
 	global token
 	url = "http://challenge.code2040.org/api/register"
 	values = {'email':'andoni@uchicago.edu', 
-				'github':'https://github.com/andonigarcia/CODE2040-API-Challenge'}
+			  'github':'https://github.com/andonigarcia/CODE2040-API-Challenge'}
 	JSONData = dumps(values).encode('utf-8')
 	req = Request(url, data = JSONData, headers = {'content-type':'application/json'})
 	resp = urlopen(req)
 	token = loads(resp.read().decode('utf-8'))
 	print("Your token is: " + token['result'])
 
-Register();
+Register()
---
+++ Python/stage_I.py
@@ -0,0 +1,57 @@
+# Andoni Garcia. 2014.
+# My source code for Stage I: Reverse a String
+
+from urllib.request import Request, urlopen
+from json import dumps, loads
+
+token = "yicZ5DfAT6"
+string = "";
+revString = "";
+
+def GetString():
+	global token, string
+	url = "http://challenge.code2040.org/api/getstring"
+	values = {'token':token}
+	JSONData = dumps(values).encode('utf-8')
+	req = Request(url, data = JSONData, headers = {'content-type':'application/json'})
+	resp = urlopen(req)
+	string = loads(resp.read().decode('utf-8'))['result']
+	print("Your string is: " + string)
+
+def ReverseString1():
+	global string, revString
+	revString = string[::-1]
+	print("The reversed string is: " + revString)
+
+# MUCH SLOWER...but uses loops (I think the point of this exercise)
+def ReverseString2():
+	global string, revString
+	for i in range(len(string)):
+		revString = string[i] + revString[:]
+	print("The reversed string is: " + revString)
+
+# Uses loops in the more cononical way (Marginally slower than above)
+def ReverseString3():
+	global string, revString
+	strlen = len(string)
+	for i in range(strlen):
+		revString = revString[:] + string[strlen - 1 - i]
+	print("The reversed string is: " + revString)
+
+def PostString():
+	global token, revString
+	url = "http://challenge.code2040.org/api/validatestring"
+	values = {'token':token,
+			  'string':revString}
+	JSONData = dumps(values).encode('utf-8')
+	req = Request(url, data = JSONData, headers = {'content-type':'application/json'})
+	resp = urlopen(req)
+	result = loads(resp.read().decode('utf-8'))['result']
+	print("Result: " + result)
+
+def StageI():
+	GetString()
+	ReverseString1()
+	PostString()
+
+StageI()
